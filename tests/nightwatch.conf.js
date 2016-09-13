53e1be36c0e481a662df84d39687230028e6001d
Zack Ferro-Fields <zackferrofields@gmail.com>
2016-09-13 14:41:52 +0100

    docker selenium nightwatch cucumberjs

---
+++ tests/nightwatch.conf.js
@@ -0,0 +1,31 @@
+const launch_url = process.env.TODOMVC_PORT_80_TCP_ADDR || 'http://localhost';
+const selenium_host = process.env.SELENIUMHUB_PORT_4444_TCP_ADDR || 'localhost';
+
+module.exports = {
+  'src_folders':[ require('nightwatch-cucumber')() ],
+  'page_objects_path': './page-objects',
+  'test_settings': {
+    'default': {
+      'launch_url': `http://${launch_url}/`,
+      'selenium_host': selenium_host,
+      'screenshots': {
+        'enabled': true,
+        'on_failure' : true,
+        'on_error' : false,
+        'path': './reports/screenshots'
+      },
+      'desiredCapabilities': {
+        'browserName': 'firefox',
+        'javascriptEnabled': true,
+        'acceptSslCerts': true
+      }
+    },
+    'chrome': {
+      'desiredCapabilities': {
+        'browserName': 'chrome',
+        'javascriptEnabled': true,
+        'acceptSslCerts': true
+      }
+    }
+  }
+};
